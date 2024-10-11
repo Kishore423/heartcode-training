c6f23091b36fe5553099f0e3d92dc0f7d6577723
Kishore423 <kishore.k.2024@comuting.smu.edu.sg>
2024-10-11 14:25:00 +0800

    h

--- src/app/server/user.ts
+++ src/app/server/user.ts
@@ -4,6 +4,6 @@ import { db } from "../../db";
 import { users } from "../../db/schema";
 
 
-export async function insertOneUser(name : String, isDrugDealer : boolean ) {
-    await db.insert(users).values({name : name , isDrugDealer : isDrugDealer});
+export async function insertOneUser(name : string, isDrugDealer : boolean ) {
+    await db.insert(users).values({ name : name , isDrugDealer : isDrugDealer});
 }
